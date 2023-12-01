c0b906ace9d3f7b589157881d78ffbb9380fe809
luisesquivia <151041173+luisesquivia@users.noreply.github.com>
2023-12-01 17:23:04 -0500

    Create section Product in view Product Page (#29)
    * Create section Product in view Product Page
    
    * Corrections for issue #27 Close
    
    * Corrections for issue #26 Close
    
    * Corrections for issue #25 Close
    
    * Corrections for issue #24 Close
    
    * Update BEM implementation in the product section
    
    * Corrections for issue #23 Close
    
    * updated product section with comments from PR
    
    * updated product section with comments from PR (media querys - js - BEM)
    
    * add and modify content in the new view product section
    
    ---------
    
    Co-authored-by: Luis Esquivia Hernández <luis.esquivia@MacBook-Pro-de-Koombea.local>

---
+++ main.js
@@ -0,0 +1,45 @@
+/* Slider vista img grande */
+const swiperPreview = new Swiper('.js-swiper-preview-img', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+
+});
+
+/* Slider vista preview */
+const swiperView = new Swiper('.js-swiper-view-img', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiperPreview,
+  }
+
+});
+
+/* Funtion Sumar input*/
+function sumar() {
+  let inputElement = document.querySelector('.js-product-add-card-number');
+  let valorActual = parseInt(inputElement.value);
+  inputElement.value = valorActual + 1;
+}
+
+let botonSumar = document.querySelector('.js-add-unit-to-product');
+botonSumar.addEventListener('click', sumar);
+
+/* Funtion Restar input */
+function restar() {
+  let inputElement = document.querySelector('.js-product-add-card-number');
+  let valorActual = parseInt(inputElement.value);
+  if (valorActual > 1) {
+    inputElement.value = valorActual - 1;
+  }
+}
+
+let botonRestar = document.querySelector('.js-substract-unit-to-product');
+botonRestar.addEventListener('click', restar);
