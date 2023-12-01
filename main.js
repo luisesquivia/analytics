32c7db91a541918c02baf4aba759575410f49526
Luis Esquivia Hernández <luis.esquivia@MacBook-Pro-de-Koombea.local>
2023-12-01 16:37:37 -0500

    add and modify content in the new view product section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Slider vista img grande */
 const swiperPreview = new Swiper('.js-swiper-preview-img', {
   loop: true,
   spaceBetween: 10,
@@ -7,6 +8,7 @@ const swiperPreview = new Swiper('.js-swiper-preview-img', {
 
 });
 
+/* Slider vista preview */
 const swiperView = new Swiper('.js-swiper-view-img', {
   loop: true,
   spaceBetween: 10,
@@ -20,16 +22,24 @@ const swiperView = new Swiper('.js-swiper-view-img', {
 
 });
 
+/* Funtion Sumar input*/
 function sumar() {
-  let inputElement = document.querySelector('.js-product__add-card-number');
+  let inputElement = document.querySelector('.js-product-add-card-number');
   let valorActual = parseInt(inputElement.value);
   inputElement.value = valorActual + 1;
 }
 
+let botonSumar = document.querySelector('.js-add-unit-to-product');
+botonSumar.addEventListener('click', sumar);
+
+/* Funtion Restar input */
 function restar() {
-  let inputElement = document.querySelector('.js-product__add-card-number');
+  let inputElement = document.querySelector('.js-product-add-card-number');
   let valorActual = parseInt(inputElement.value);
   if (valorActual > 1) {
     inputElement.value = valorActual - 1;
   }
 }
+
+let botonRestar = document.querySelector('.js-substract-unit-to-product');
+botonRestar.addEventListener('click', restar);
