bcbc2b40193a7d1d93accd9b9596f9f06b6cc3a9
Luis Esquivia Hernández <luis.esquivia@MacBook-Pro-de-Koombea.local>
2023-12-06 14:07:45 -0500

    create new features to cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,4 +1,11 @@
-let purchaseDetail = document.querySelector('.js-purchase-detail');
+const CART = {
+  products: new Map(),
+  subtotal: 0,
+  fee: 0,
+  total: 0,
+}
+
+const purchaseDetail = document.querySelector('.js-purchase-detail');
 
 /* Funtion Sumar input*/
 function sumar(target) {
@@ -11,24 +18,57 @@ function sumar(target) {
 function restar(target) {
   let inputElement = target.parentElement.querySelector('.js-product-add-card-number');
   let valorActual = parseInt(inputElement.value);
+
   if (valorActual > 1) {
     inputElement.value = valorActual - 1;
-  };
+  }
 };
 
-purchaseDetail.addEventListener('click', (event) => {
-  console.log(event)
-  let target = event.target
-  if (target.tagName === 'BUTTON') {
-    if (target.dataset.action === 'add-to-cart') {
-      sumar(target);
-    }
+/* Funtion updateTotalPrice input */
+function updateTotalPrice() {
+  const $totalPrice = document.querySelector('.js-summary-total-price');
+  let total = 0;
+  const elementos = document.querySelectorAll('.js-product-details');
 
-    if (target.dataset.action === 'substract-to-cart') {
-      restar(target);
-    }
-  };
+  elementos.forEach(elemento => {
+    const precio = parseFloat(elemento.getAttribute('data-product-price'));
+    total += precio;
+  });
+
+  CART.products.forEach((product) => {
+    total += product
+  });
+
+  $totalPrice.textContent = `$${total}`;
+};
+
+purchaseDetail.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('js-add-unit-to-product')) {
+    const $parent = target.closest('.js-product-details');
+    const $amount = $parent.querySelector('.js-product-add-card-number');
+    const { productId, productPrice } = $parent.dataset;
+    const amount = parseInt($amount.value ) + 1;
+
+    CART.products.set(productId, parseInt(productPrice) * amount);
+
+    sumar(target);
+    updateTotalPrice();
+  } else if (target.classList.contains('js-substract-unit-to-product')) {
+    const $parent = target.closest('.js-product-details');
+    const $amount = $parent.querySelector('.js-product-add-card-number');
+    const { productId, productPrice } = $parent.dataset;
+    const amount = parseInt($amount.value ) - 1;
+
+    CART.products.set(productId, parseInt(productPrice) * amount);
+
+    updateTotalPrice();
+    restar(target);
+  } 
 });
+
+window.onload = function() {
+  updateTotalPrice();
+};
 
 const swiper = new Swiper('.js-swiper', {
   navigation: {
@@ -53,12 +93,38 @@ const swiper = new Swiper('.js-swiper', {
       spaceBetween: 40,
     },
     920: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+      slidesPerView: 5,
+      spaceBetween: 30,
     },
+
     1240: {
       slidesPerView: 5,
       spaceBetween: 20,
     },
+
+    1410: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+
+    1420: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+
+    1430: {
+      slidesPerView: 3,
+      spaceBetween: 12,
+    },
+
+    1460: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+
+    1490: {
+      slidesPerView: 3,
+      spaceBetween: 45,
+    }
   }
 });
